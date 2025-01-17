d462c782cb2a915fc66063c1694529030513fce1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-17 14:36:45 -0500

    collectDirectoryPackages: be more pure We previously aded `llakaLib` by default. But, since we're moving to true purity, we should have the user add this kind of thing manually via the `extras` parameter.. We can make a non-pure function injecting `llakaLib` by default in the future.

--- lib/collectDirectoryPackages.nix
+++ lib/collectDirectoryPackages.nix
@@ -1,17 +1,14 @@
-{ lib, llakaLib }:
+{ lib }:
 # From https://noogle.dev/f/lib/packagesFromDirectoryRecursive#create-a-scope-for-the-nix-files-found-in-a-directory
 # For convenience in flake with custom packages relying on each other
-# We also let custom packages rely on llakaLib functions
+# Doesn't insert llakaLib by default, you can do that on your own via `extras`
 
-let
-  utils = { inherit llakaLib; };
-in
-{ directory, pkgs }: # Function arguments
+{ directory, pkgs, extras ? {} }: # Function arguments
 lib.makeScope pkgs.newScope
 (
   selfPkgs: lib.packagesFromDirectoryRecursive # selfPkgs are internally-defined packages we find along the way
   {
-    callPackage = lib.callPackageWith (selfPkgs // pkgs // utils ); # Rely on custom packages, `pkgs`, and `llakaLib`
+    callPackage = lib.callPackageWith (selfPkgs // pkgs // extras ); # Rely on custom packages, `pkgs`, and `llakaLib`
     inherit directory;
   }
 )
